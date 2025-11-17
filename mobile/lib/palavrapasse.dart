47b5ca19b5166cc54db79cab3f4623a11766cc89
LihSlay <pv27817@alunos.estgv.ipv.pt>
2025-11-17 17:48:47 +0000

    criação dos ficheiros para a página das definições, alteração de palavra passe e alteração de idioma

---
+++ mobile/lib/palavrapasse.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Clinimolelos',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Palavrapasse(title: 'Alterar Palavra-passe'),
+    );
+  }
+}
+
+class Palavrapasse extends StatelessWidget {
+  final String title;
+
+  const Palavrapasse({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          title,
+          style: TextStyle(color: Colors.white), // título branco
+        ),
+        elevation: 0,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF907041), // início
+                Color(0xFFA68A69), // cor final
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+      ),
+      body: Center(child: Text("Botões")),
+    );
+  }
+}
