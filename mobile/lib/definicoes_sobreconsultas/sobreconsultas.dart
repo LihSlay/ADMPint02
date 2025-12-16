c624ae4c4bb44c61a0125ca9ed9633482ed4a051
LihSlay <pv27817@alunos.estgv.ipv.pt>
2025-12-16 10:06:53 +0000

    alteração sobre consultas

--- mobile/lib/definicoes_sobreconsultas/sobreconsultas.dart
+++ mobile/lib/definicoes_sobreconsultas/sobreconsultas.dart
@@ -44,7 +44,7 @@ class SobreConsultasPage extends StatelessWidget {
               borderRadius: BorderRadius.circular(8),
               boxShadow: [
                 BoxShadow(
-                  color: Colors.black.withOpacity(0.10),
+                  color: Colors.black.withValues(alpha: 0.10),
                   blurRadius: 6,
                   offset: const Offset(0, 3),
                 ),
